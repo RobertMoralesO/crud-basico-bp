4bda668748d1075894f8cd977357cbecdf146f08
Roberto Morales <roberto@morales.com.co>
2022-10-10 19:45:38 -0500

    interfaz formulario casi casi

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Formulario = () => {
+  return (
+      <div className="container mt-5">
+          <h1 className="text-center">CRUD BÁSICO BUENAS PRÁCTICAS</h1>
+          <hr/>
+          <div className="row">
+              <div className="col-8">
+                  <h4 className="text-center">Listado de Frutas</h4>
+                  <ul className="list-group">
+                      {
+                          //Listar las frutas
+                      }
+                  </ul>
+              </div>
+
+              <div className="col-4">
+                  <h4 className="text-center">
+                      Agregar Frutas
+                  </h4>
+                  <form>
+                      <input type="text" 
+                      className="form-control mb-2"
+                      placeholder='Ingrese Fruta'
+                      />
+                       <input type="text" 
+                      className="form-control mb-2"
+                      placeholder='Ingrese Descripción'
+                      
+                      />
+                      <button className=" btn btn-primary btn-block"
+                      type='submit'>Agregar</button>
+                  </form>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+export default Formulario
